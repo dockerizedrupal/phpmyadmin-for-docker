cb190052b4f2b876eb3692f64a49d2bd41a23bbc
Jürgen Viljaste <j.viljaste@gmail.com>
2014-11-27 13:37:08 +0200

    Update

--- src/build/modules/phpmyadmin/manifests/init.pp
+++ src/build/modules/phpmyadmin/manifests/init.pp
@@ -2,25 +2,19 @@ class phpmyadmin {
   require phpmyadmin::httpd
   require phpmyadmin::php
 
-  file { '/var/www/phpMyAdmin-4.2.12-all-languages.zip':
+  file { '/tmp/phpMyAdmin-4.2.12-all-languages.zip':
     ensure => present,
-    source => 'puppet:///modules/phpmyadmin/var/www/phpMyAdmin-4.2.12-all-languages.zip'
+    source => 'puppet:///modules/phpmyadmin/tmp/phpMyAdmin-4.2.12-all-languages.zip'
   }
 
   exec { 'unzip phpMyAdmin-4.2.12-all-languages.zip':
-    cwd => '/var/www',
+    cwd => '/tmp',
     path => ['/usr/bin'],
-    require => File['/var/www/phpMyAdmin-4.2.12-all-languages.zip']
+    require => File['/tmp/phpMyAdmin-4.2.12-all-languages.zip']
   }
 
-  exec { 'rm phpMyAdmin-4.2.12-all-languages.zip':
-    cwd => '/var/www',
-    path => ['/bin'],
-    require => Exec['unzip phpMyAdmin-4.2.12-all-languages.zip']
-  }
-
-  exec { 'mv phpMyAdmin-4.2.12-all-languages phpmyadmin':
-    cwd => '/var/www',
+  exec { 'mv phpMyAdmin-4.2.12-all-languages /var/www/phpmyadmin':
+    cwd => '/tmp',
     path => ['/bin'],
     require => Exec['unzip phpMyAdmin-4.2.12-all-languages.zip']
   }
