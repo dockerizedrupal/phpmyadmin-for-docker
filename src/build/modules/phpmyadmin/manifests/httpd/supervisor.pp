697ad3ddc6806387e4ac5bd6ce59f68c5b3f0e8a
Jürgen Viljaste <j.viljaste@gmail.com>
2014-11-29 21:44:33 +0200

    Update

--- src/build/modules/phpmyadmin/manifests/httpd/supervisor.pp
+++ src/build/modules/phpmyadmin/manifests/httpd/supervisor.pp
@@ -1,6 +1,7 @@
 class phpmyadmin::httpd::supervisor {
   file { '/etc/supervisor/conf.d/httpd.conf':
     ensure => present,
-    source => 'puppet:///modules/phpmyadmin/etc/supervisor/conf.d/httpd.conf'
+    source => 'puppet:///modules/phpmyadmin/etc/supervisor/conf.d/httpd.conf',
+    mode => 644
   }
 }
