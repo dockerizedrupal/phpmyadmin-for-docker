b8b40a7eb968046832cd80f25a40529dfb93ec81
Jürgen Viljaste <j.viljaste@gmail.com>
2014-12-07 20:43:11 +0200

    Update

--- src/build/modules/phpmyadmin/manifests/packages.pp
+++ src/build/modules/phpmyadmin/manifests/packages.pp
@@ -1,13 +1,8 @@
 class phpmyadmin::packages {
-  exec { 'apt-get update':
-    path => ['/usr/bin']
-  }
-
   package {[
       'php5',
       'php5-mysql'
     ]:
-    ensure => present,
-    require => Exec['apt-get update']
+    ensure => present
   }
 }
