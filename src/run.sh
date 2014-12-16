81e87387d8e01cf689b6347363175399cda65f09
Jürgen Viljaste <j.viljaste@gmail.com>
2014-12-16 16:13:40 +0200

    Update

--- src/run.sh
+++ src/run.sh
@@ -8,6 +8,12 @@ export FACTER_DB_PORT="$(echo "${DB_PORT}" | cut -d ":" -f2)"
 export FACTER_DB_USERNAME="${DB_USERNAME}"
 export FACTER_DB_PASSWORD="${DB_PASSWORD}"
 
+if [ -z "${SERVER_NAME}" ]; then
+  SERVER_NAME="localhost"
+fi
+
+export FACTER_SERVER_NAME="${SERVER_NAME}"
+
 puppet apply --modulepath=/src/run/modules /src/run/run.pp
 
 /usr/bin/supervisord
