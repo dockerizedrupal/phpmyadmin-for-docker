25fbf97c9aae8ba11b0f3a9f3a9e4eb0aebd7788
Jürgen Viljaste <j.viljaste@gmail.com>
2014-12-18 19:59:59 +0200

    Update

--- src/run/modules/phpmyadmin/manifests/init.pp
+++ src/run/modules/phpmyadmin/manifests/init.pp
@@ -1,5 +1,7 @@
 class phpmyadmin {
-  require phpmyadmin::httpd::ssl
+  if ! file_exists('/phpmyadmin/ssl/certs/vhost.crt') {
+    require phpmyadmin::httpd::ssl
+  }
 
   if $db_host {
     file { '/phpmyadmin/data/config.inc.php':
