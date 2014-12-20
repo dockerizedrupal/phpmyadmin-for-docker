3098f18d54e5fa8cd8076789faee48287e22f417
Jürgen Viljaste <j.viljaste@gmail.com>
2014-12-21 01:46:37 +0200

    Update

--- src/build/modules/phpmyadmin/manifests/httpd/packages.pp
+++ src/build/modules/phpmyadmin/manifests/httpd/packages.pp
@@ -1,20 +1,8 @@
 class phpmyadmin::httpd::packages {
-  file { '/etc/apt/sources.list.d/non-free.list':
-    ensure => present,
-    source => 'puppet:///modules/phpmyadmin/etc/apt/sources.list.d/non-free.list',
-    mode => 644
-  }
-
-  exec { 'apt-get update':
-    path => ['/usr/bin'],
-    require => File['/etc/apt/sources.list.d/non-free.list']
-  }
-
   package {[
       'apache2',
       'libapache2-mod-fastcgi'
     ]:
-    ensure => present,
-    require => Exec['apt-get update']
+    ensure => present
   }
 }
